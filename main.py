05ebe06b066e7e11040302c4687a3aaff8b57c91
ceciliacal <c.calavaro@yahoo.it>
2020-12-05 16:50:34 +0100

    iterare KNN per tutte le colonne + fatto kkn anche per n/a

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ import pandas as pd
 import tensorflow as tf
 import matplotlib
 from matplotlib import pyplot as plt
-from matplotlib.cbook import boxplot_stats
-from sklearn.neighbors import NearestNeighbors
+from sklearn.impute import KNNImputer
+from sklearn.neighbors import KNeighborsRegressor
 
 import seaborn as sns
 
@@ -25,23 +25,15 @@ def print_hi(name):
 
 def openFiles(datasetPath):
 
-    '''
-    column_names2 = []
-    string = "F"
-    for i in range(1,21):
-        column_names2.append(string + str(i))
-
-    print("colonne: " , column_names2)
-    '''
-    
     # leggiamo i dati specificando le colonne opportune
+    #TODO: dataframe = read_csv(url, header=None, na_values='?')
     dataset = pd.read_csv(datasetPath)
 
     print("Shape:", dataset.shape)
     print(dataset.tail())
 
 
-     # Ora dividiamo il dataset in training set e test set secondo le proporzioni 80-20
+    # Ora dividiamo il dataset in training set e test set secondo le proporzioni 80-20
     train_dataset = dataset.sample(frac=0.8, random_state=0)
     test_dataset = dataset.drop(train_dataset.index)
 
@@ -51,100 +43,77 @@ def openFiles(datasetPath):
     print("train_dataset shape:", train_dataset.shape, "train_labels shape:", train_labels.shape)
     print("test_dataset shape:", test_dataset.shape, "test_labels shape:", test_labels.shape)
 
-    # calcoliamo il numero di valori mancanti su train e test
-    summary_train = get_na_count(train_dataset)
-    print("count NaN TRAINING: ",summary_train,"\n\n\n")
-    summary_test = get_na_count(test_dataset)
-    print("count NaN TESTING: ",summary_test,"\n\n\n")
+    # calcoliamo il numero di valori mancanti su train e test (n/a)
+    train_dataset=naDetection(train_dataset)
+    test_dataset=naDetection(test_dataset)
 
-    #print(train_dataset['F1'].mean())
-
-    print("\n\nMEDIA PER OGNI ATTRIBUTO: ")
-    # calcoliamo media di ogni colonna e la sostituiamo ai valori mancanti
-
-    string = "F"
-    for i in range(1, 21):
-        currColumn = string + str(i)
-        currMean = train_dataset[currColumn].mean()
-
-        print(currColumn,": ", currMean)
-
-        train_dataset[currColumn] = train_dataset[currColumn].fillna(currMean)
-        test_dataset[currColumn] = test_dataset[currColumn].fillna(currMean)
-
-    # controlliamo nuovamente train e test
+    # controlliamo nuovamente che train e test siano senza n/a
     summary_train = get_na_count(train_dataset)
     print(summary_train)
     summary_test = get_na_count(test_dataset)
     print(summary_test)
 
 
-    # Visualizziamo i dati tramite il pairplot seaborn le due classi di colore diverso usando l'attributo 'hue'
 
-
-    # data2 = [18, 19, 19, 20, 20, 20, 20, 20, 21, 21, 21, 22, 23, 25, 28, 30, 31, 37]
+    #ORA FACCIAMO BOX PLOT !!!!!!!!!!!
+    #data2 = [18, 19, 19, 20, 20, 20, 20, 20, 21, 21, 21, 22, 23, 25, 28, 30, 31, 37]
     #data2 = [5, 39, 75, 79, 85, 90, 91, 93, 93, 98]
-    data2 = []
+
+
+    dataColumn = np.array([])
 
     #inserisco istanze della colonna in una lista
     print("size colonna: ",train_dataset['F1'].size)
+
     for i in train_dataset['F1']:
-        data2.append(i)
+        dataColumn = np.append(dataColumn,i)
 
-
-    df = pd.DataFrame(data2)
-    print("df: ", df)
-    #ax = sns.boxplot(data2=df)
-
-    fig1, ax = plt.subplots()
-    ax.set_title('Basic Plot')
-    ax.boxplot(data2)
+    title = 'F1 before KNN'
+    print("\n\ndata2: ",dataColumn,"\n\n")
+    createBoxplot(dataColumn,title)
 
 
 
-    median = np.median(data2)
-    q3 = np.percentile(data2, 75)   #upper_quartile
-    q1 = np.percentile(data2, 25)   #lower_quartile
-    iqr = q3 - q1
+def naDetection(dataset):
 
-    print("mediana: ", median)
-    print("q1: ", q1)
-    print("q3: ", q3)
-    print("iqr: ", iqr)
+    df = pd.DataFrame(dataset)
+    imputer = KNNImputer(n_neighbors=2)
+    imputed = imputer.fit_transform(df)
+    df_imputed = pd.DataFrame(imputed)
 
-    l= q1-1.5*iqr
-    r=q3+1.5*iqr
-    print("l: ", l,"    r:",r)
-    count =0
+    #print(" ------->", get_na_count(df_imputed))
 
-    for i in data2:
-        if i <l or i > r:
-            count=count+1
-            print ("-- outlier n ",count,":  ",i)
-
-    ax.set_xlim(right=1.5)
-    plt.show()
+    return changeColNames(df_imputed)
 
 
+def changeColNames(df_imputed):
+    #print("TOTALE PRIMA-   ", df_imputed)
+    #print("COLONNA 0-   ", df_imputed[0])
 
-from sklearn.neighbors import KNeighborsRegressor
+    string = "F"
+    for i in range(1, 21):
+        currColumn = string + str(i)
+        index = i - 1
+        #print("index: ", index)
+        # print(df_imputed.rename(columns={index: 'F1'}))
+        df_imputed.rename(columns={index: currColumn}, inplace=True)
 
-def prova_knn():
+    print("TOTALE DOPO-   ", df_imputed)
+    return df_imputed
 
-    # dataset (X=m^2, y=rental price)
 
-    data2 = np.array([18, 19, 19, 20, 20, 20, 20, 20, 21, 21, 21, 22, 23, 25, 28, 30, 31, 37])
 
-    df = pd.DataFrame(data2)
-    #print("df: ", df)
+def createBoxplot(dataset, title):
+    #data2 = np.array([18, 19, 19, 20, 20, 20, 20, 20, 21, 21, 21, 22, 23, 25, 28, 30, 31, 37])
+    #data2 = np.array([5, 39, 75, 79, 85, 90, 91, 93, 93, 98])
 
     fig1, ax = plt.subplots()
-    ax.set_title('before KNN ')
-    ax.boxplot(data2)
+    ax.set_title(title)
+    ax.boxplot(dataset)
 
-    median = np.median(data2)
-    q3 = np.percentile(data2, 75)  # upper_quartile
-    q1 = np.percentile(data2, 25)  # lower_quartile
+    median = np.median(dataset)
+    q3 = np.percentile(dataset, 75)  # upper_quartile
+    q1 = np.percentile(dataset, 25)  # lower_quartile
     iqr = q3 - q1
 
     print("mediana: ", median)
@@ -156,55 +125,69 @@ def prova_knn():
     r = q3 + 1.5 * iqr
     print("l: ", l, "    r:", r)
 
-
-
-    #trovo l'outlier
-    out = 0
+    #trovo gli outliers e li inserisco in una lista
+    outliers = []
     count = 0
-    for i in data2:
+    for i in dataset:
         if i < l or i > r:
             count = count + 1
-            out=i
-            print("-- outlier n ", count, ":  ", out)
+            outliers.append(i)
+            print("-- outlier n ", count, ":  ", outliers[count - 1])
 
     ax.set_xlim(right=1.5)
     plt.show()
 
+    return outliers
+
+
+
+def knnDetection():
+
+    #data2 = np.array([18, 19, 19, 20, 20, 20, 20, 20, 21, 21, 21, 22, 23, 25, 28, 30, 31, 37])
+    data2 = np.array([5, 39, 75, 79, 85, 90, 91, 93, 93, 98])
+    title = 'before KNN'
+
+    outliers = createBoxplot(data2, title)
+
+    # copio dataset in lista y e tolgo outliers
+    y = data2.copy()
+    #print("y = ", y)
+    for i in outliers:
+        #print ("i= ",i)
+        #y.remove(i)
+        y = y[y != i]
+        #print("y = ", y)
+
+    print("y = ", y)
+    print("data2: ", data2)
+
     # ============ FINORA ABBIAMO BOXPLOT ==========
-    # TODO: in funzione a parte che ritorna lista di outliers
     # TODO: capire come sceglire K
     # TODO: cambiare anche N/A con funzione che implementa algoritmo KNN
+
     # ORA ABBIAMO TOLTO E SOSTITUITO OUTLIER : USIAMO KNN !!!!!
 
-    lenX = len(data2)-count
+    lenX = len(data2)-len(outliers)
     rows = lenX
     col = 1
-
-    X = [[0 for i in range(col)] for j in range(rows)]
-    X[0][0] = 33333
-
+    X = [[0 for i in range(col)] for j in range(rows)]      #inizializzo X come lista 2D
     count_X_position = 0
 
-    #metto dati nella lista 2D
+    #metto dati nella lista 2D "X"
 
-    for i in data2:
-        if i!=out:
+    #per creare lista 2D "X" per poterla usare in KNN in cui devono andarci tutti i valori di data2 tranne outliers
+    #così poi a KNN gli do X senza outlier che gli passo separatamente, in modo da calcolare media dei k vicini e sostituirli
+    for i in y:
 
-            #print("count X = ", count_X_position,"     i = ",i)
-            X[count_X_position][0]=i
-            #print("X[count_X_position][0] = ", X[count_X_position][0])
-            count_X_position = count_X_position + 1
-
+        #print("count X = ", count_X_position,"     data2_elem = ",i)
+        X[count_X_position][0]=i
+        #print("X[count_X_position][0] = ", X[count_X_position][0])
+        count_X_position = count_X_position + 1
 
 
     print("X = ", X)
 
-    #copio dataset in y e tolgo outliers
-    y = data2.copy()
-    y = y[y!=out]
-    #y.remove(out)
-    print("y = ", y)
-    print("data2: ", data2)
+
 
 
     '''
@@ -219,51 +202,25 @@ def prova_knn():
     neigh.fit(X, y)
 
     # predict
-    result = neigh.predict([[37]])
-    print("result: ", result[0])
+    result = []
+    for i in outliers:
+        result.append(neigh.predict([[i]]))
+    print("result: ", result[0][0],result[1][0])
 
 
-    #il risultato va al posto di 37 in data2
-    data2[data2 == out] = round(result[0])
-    print("data2: ", data2)
+    #sostuituiamo i risultati con gli outliers nel dataset originario
+    for i in outliers:
+        for j in result:
+            data2[data2 == i] = (j)
+            print("data2: ", data2)
+
+
 
     # ============= BOXPLOT X VEDERE CHE FUNZIONA ===========
-    #TODO : da eliminare !
-    df = pd.DataFrame(data2)
-    # print("df: ", df)
-
-    fig1, ax = plt.subplots()
-    ax.set_title('after KNN ')
-    ax.boxplot(data2)
-
-    median = np.median(data2)
-    q3 = np.percentile(data2, 75)  # upper_quartile
-    q1 = np.percentile(data2, 25)  # lower_quartile
-    iqr = q3 - q1
-
-    print("mediana: ", median)
-    print("q1: ", q1)
-    print("q3: ", q3)
-    print("iqr: ", iqr)
-
-    l = q1 - 1.5 * iqr
-    r = q3 + 1.5 * iqr
-    print("l: ", l, "    r:", r)
-
-    # trovo l'outlier
-    out = 0
-    count = 0
-    for i in data2:
-        if i < l or i > r:
-            count = count + 1
-            out = i
-            print("-- outlier n ", count, ":  ", out)
-
-    ax.set_xlim(right=1.5)
-    plt.show()
-
-
-
+    title = 'after KNN'
+    if len(createBoxplot(data2,title)) == 0:
+        print("KNN terminato, outliers sostituiti")
+        return 0
 
 
 
@@ -283,13 +240,12 @@ def get_na_count(dataset):
 
 
 def main():
-    print ("ciaomerda")
     print("tensorflow: ", tf.__version__)
 
-    dataset_path = './auto-mpg.data'
     datasetPath = './training_set.csv'
-    prova_knn()
-    #openFiles(datasetPath)
+    #knnDetection()
+    #naDetection()
+    openFiles(datasetPath)
 
 
 
